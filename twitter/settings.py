711918fdb2fa75d3dca066eae5a8c4c77219ae19
Mervan Koncuk <mervankoncuk@gmail.com>
2024-03-04 22:26:39 +0300

    change debug

--- twitter/settings.py
+++ twitter/settings.py
@@ -13,13 +13,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#0)l6d3-*i8%g8=)d6qtf5i-^*f1u$*di*2vb4l#!m)7a^2m@6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
 
-
-# Application definition
 
 INSTALLED_APPS = [
     'posts',
